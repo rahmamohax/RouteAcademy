9e065123745479f1dcea3815708e734f472044e1
rahma2005 <128816631+rahma2005@users.noreply.github.com>
2025-07-21 22:14:42 +0300

    OOP1 Assignment

---
+++ OOP01/employees/Employees.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Program;
+
+namespace employees
+{
+    public class Employees
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        HiringDate HiringDate { get; set; }
+        public decimal Salary { get; set; }
+        Gender Gender { get; set; }
+        SecurityPrivileges Privileges { get; set; }
+
+        public Employees(int id, string name, Gender gender, decimal salary, HiringDate hiringD,   SecurityPrivileges SecLevel)
+        {
+            ID = id;
+            Name = name;
+            HiringDate = hiringD;
+            Salary = salary;
+            Gender = gender;
+            Privileges = SecLevel;            
+        }
+
+        public Employees()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}\n" +
+                $"Name: {Name}\n" +
+                $"HiringDate: {HiringDate}\n" +
+                $"Salary: {Salary}\n" +
+                $"Gender: {Gender}\n" +
+                $"Security Level: {Privileges}";
+        }
+    }
+}
---
+++ task/task/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Number of Tasks: ");
+        int num = int.Parse(Console.ReadLine());
+        int[] id = new int[num];
+        string[] Title = new string[num];
+        string[] desc = new string[num];
+
+        for (int i = 0; i < num; i++) {
+            Console.Write($"Input ID of Task {i + 1}: ");
+            int test = int.Parse(Console.ReadLine());
+            foreach (int x in id)
+            {
+                if (x == test)
+                {
+                    Console.WriteLine("ID must be unique");
+                    break;
+                }
+                else id[i] = test;
+            }
+            Console.WriteLine("Type Task's Title: ");
+            Title[i] = Console.ReadLine();
+
+            
+            do
+            {
+                Console.WriteLine("Do you wants to add description? (Y/N)");
+                char des = char.Parse(Console.ReadLine());
+
+                if (des == 'Y' || des == 'y')
+                {
+                    desc[i] = Console.ReadLine();
+                    break;
+                }
+                else if (des == 'N' || des == 'n')
+                {
+                    desc[i] = null;
+                    break;
+                }
+                else Console.WriteLine("Invalid Input");
+            }
+            while (true);
+            
+        }
+
+        for (int i = 0; i < num; i++) {
+            Console.WriteLine($"Task {i+1}'s ID: {id[i]}");
+            Console.WriteLine($"Task {i + 1}'s Title: {Title[i]}");
+            if(desc[i] != null) 
+                Console.WriteLine($"Task {i + 1}'s Description: {desc[i]}");
+
+        }
+
+    }
+}
