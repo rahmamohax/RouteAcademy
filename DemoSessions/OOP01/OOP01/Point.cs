0b612eed45335b43d3dd466c08397b4696aa4b69
rahma2005 <rhmarhma360@gmail.com>
2025-08-07 22:03:14 +0300

    save changes

---
+++ DemoSessions/OOP01/OOP01/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP01
+{
+    internal class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Point(double _X, double _Y)
+        {
+            X = _X;
+            Y = _Y;
+        }
+
+    }
+}
