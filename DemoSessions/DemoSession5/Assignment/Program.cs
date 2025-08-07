5c8918b323491dc1d5d248245cb8a51c9996074f
rahma2005 <rhmarhma360@gmail.com>
2025-08-07 20:44:57 +0300

    assignment's part 1

---
+++ DemoSessions/DemoSession5/Assignment/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Assignment
+{
+    public class Programm
+    { 
+        static void Main(string[] args)
+        {
+            #region Part 01
+            #region Question 1: What is the primary purpose of an interface in C#?
+            //b) To define a blueprint for a class
+            //Interfaces defines a contract(blueprint) that classes must follow
+            #endregion
+
+            #region Question 2: Which of the following is NOT a valid access modifier for interface members in C#?
+            //a) private
+            //all members are  implicitly public
+            #endregion
+
+            #region Question 3: Can an interface contain fields in C#?
+            //b) No
+            //It containes only field's signature
+            #endregion
+
+            #region Question 4: In C#, can an interface inherit from another interface?
+            //b) Yes, interfaces can inherit from multiple interfaces
+            #endregion
+
+            #region Question 5: Which keyword is used to implement an interface in a class in C#?
+            //None of the above
+            //classes use a colon (:) to implement interfaces.
+            #endregion
+
+            #region Question 6: Can an interface contain static methods in C#?
+            //b) NO
+            #endregion
+
+            #region Question 7: In C#, can an interface have explicit access modifiers for its members?
+            //b) No, all members are implicitly public
+            //All interface members are automatically public.
+            #endregion
+
+            #region Question 8: What is the purpose of an explicit interface implementation in C#?
+            //b) To provide a clear separation between interface and class members
+
+            #endregion
+
+            #region Question 9: In C#, can an interface have a constructor?
+            //b) No, interfaces cannot have constructors
+            #endregion
+
+            #region Question 10: How can a C# class implement multiple interfaces?
+            //c) By separating interface names with commas
+            #endregion
+            #endregion
+        }
+    }
+}
