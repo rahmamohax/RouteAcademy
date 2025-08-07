1598988aca107cd57d087021fc4606403a5af623
rahma2005 <rhmarhma360@gmail.com>
2025-08-07 21:47:45 +0300

    Part 2

--- DemoSessions/DemoSession5/Assignment/Program.cs
+++ DemoSessions/DemoSession5/Assignment/Program.cs
@@ -1,4 +1,10 @@
-﻿namespace Assignment
+﻿using Assignment.Question_2;
+using Assignment.Question_3;
+using System;
+using System.Data;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Assignment
 {
     public class Programm
     { 
@@ -50,7 +56,77 @@
             #region Question 10: How can a C# class implement multiple interfaces?
             //c) By separating interface names with commas
             #endregion
+
             #endregion
-        }
+
+            #region Part 02
+            #region Question 01: Define an interface named IShape with a property Area and a method DisplayShapeInfo.Create two interfaces, ICircle and IRectangle, that inherit from IShape.Implement these interfaces in classes Circle and Rectangle. Test your implementation by creating instances of both classes and displaying their shape information.
+            //Circle circle = new Circle(2.5);
+            ////circle.Area = 10; // Invalid, readonly
+            //circle.DisplayShapeInfo();
+
+            //Rectangle rectangle = new Rectangle(20, 10);
+            //rectangle.DisplayShapeInfo();
+
+            #endregion
+
+            #region Question 02:
+            // defining the IAuthenticationService interface with two methods: AuthenticateUser and AuthorizeUser.
+            //BasicAuthenticationService class implements this interface and provides the specific implementation for these methods.
+
+            //In the BasicAuthenticationService class, the AuthenticateUser method compares the provided username and password with the stored credentials
+            //It returns true if the user is authenticated and false otherwise
+
+            //The AuthorizeUser method checks if the user with the given username has the specified role.
+            //It returns true if the user is authorized and false otherwise.
+
+            //In the Main method, we create an instance of the BasicAuthenticationService class and assign it to the authService variable of type IAuthenticationService.We then call the AuthenticateUser and AuthorizeUser methods using this interface reference.
+
+            //IAuthenticationService authentication = new BasicAuthenticationService();
+
+            //string username = "admin";
+            //string password = "1234";
+            //string role = "Administrator";
+
+            //bool isAuthenticated = authentication.AuthenticateUser(username, password);
+            //Console.WriteLine($"Is 'admin' authenticated? {isAuthenticated}");
+            //bool isAuthorized = authentication.AuthorizeUser(username, role);
+            //Console.WriteLine($"Is 'admin' authorized? {isAuthorized}");
+            //Console.WriteLine();
+
+            //string username2 = "Ali";
+            //string password2 = "Pass13";
+            //string role2 = "Manager";
+
+            //isAuthenticated = authentication.AuthenticateUser(username2, password2);
+            //Console.WriteLine($"Is 'Ali' authenticated? {isAuthenticated}");
+            //isAuthorized = authentication.AuthorizeUser(username2, role2);
+            //Console.WriteLine($"Is 'Ali' authorized? {isAuthorized}");
+
+            #endregion
+
+            #region Question 03
+            //we define the INotificationService interface with a method SendNotification that takes a recipient and a message as parameters.
+            //We then create three classes: EmailNotificationService, SmsNotificationService, and PushNotificationService, which implement the INotificationService interface.
+
+            //In each implementation, we provide the logic to send notifications through the respective communication channel:
+            //The EmailNotificationService class simulates sending an email by outputting a message to the console.
+            //The SmsNotificationService class simulates sending an SMS by outputting a message to the console.
+
+
+
+            //INotificationService emailService = new EmailNotificationService();
+            //INotificationService smsService = new SmsNotificationService();
+            //INotificationService pushService = new PushNotificationService();
+
+            //emailService.SendNotification("rahma@example.com", "Welcome to our platform!");
+            //smsService.SendNotification("+201234567890", "Your verification code is 123456.");
+            //pushService.SendNotification("rahmaDevice", "You have a new message.");
+         
+
+        #endregion
+            #endregion
+
     }
+}
 }
---
+++ DemoSessions/DemoSession5/Assignment/Question 3/SmsNotificationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Question_3
+{
+    internal class SmsNotificationService : INotificationService
+    {
+        public void SendNotification(string recipient, string? message)
+        {
+            Console.WriteLine($"SMS Message sent to {recipient}: {message}");
+        }
+    }
+}
