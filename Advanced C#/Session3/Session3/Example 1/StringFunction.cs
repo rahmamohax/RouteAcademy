744b676b39a7a3092ee8fe850bf6e07c17b576f6
rahma2005 <rhmarhma360@gmail.com>
2025-08-29 18:15:22 +0300

    Example 1

---
+++ Advanced C#/Session3/Session3/Example 1/StringFunction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session3.Example_1
+{
+    internal class StringFunction
+    {
+        public static int GetCountOfUpperChar(string? str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsUpper(str[i])) count++;
+
+            return count;
+        }
+        public static int GetCountOfLowerChar(string? str)
+        {
+            int count = 0;
+            if (!string.IsNullOrWhiteSpace(str))
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsLower(str[i])) count++;
+
+            return count;
+        }
+    }
+}
--- Advanced C#/Session3/Session3/Program.cs
+++ Advanced C#/Session3/Session3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Session3.Example_1;
+
+namespace Session3
+{
+    class Program
+    {
+        //Delegates: a type that represents refreneces to methods
+        public delegate int StringFuncDelegate(string? s);
+        //new Delegate (class)
+        //Reference from this delegate can refer to:
+        //one or more function (pointer)
+        //fuction must match the signature of delegate (parameter list - return type)
+
+        public static void Main(string[] args)
+        {
+            #region Example 1
+            //Step 01. Decleare Reference from delegate
+            //Step 02. Initialize reference => point to a function
+
+            //StringFuncDelegate stringFunc = new StringFuncDelegate(StringFunction.GetCountOfUpperChar);
+            //StringFuncDelegate stringFunc = StringFunction.GetCountOfUpperChar; //Syntax Sugar
+
+            //stringFunc += StringFunction.GetCountOfLowerChar; // Add New Reference for function
+
+            //stringFunc -= StringFunction.GetCountOfUpperChar;
+
+            //Step 03. Use Delegate Reference to [Call [Invoke] Method]
+
+            //int? res = stringFunc?.Invoke("RoUtE");
+            //if (stringFunc is not null)
+            //{
+            //    int res = stringFunc("RoUtE"); //Syntax Sugar
+            //    Console.WriteLine(res);
+            //}    
+            #endregion
+
+
+        }
+    }
+}
